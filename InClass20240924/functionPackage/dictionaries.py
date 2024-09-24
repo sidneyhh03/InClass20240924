dcdb6db58f345f463cb9adab9e885bda7c03a75b
nicomp42 <nicholdw@ucmail.uc.edu>
2024-09-24 08:57:52 -0400

    Made changes during class

--- InClass20240924/functionPackage/dictionaries.py
+++ InClass20240924/functionPackage/dictionaries.py
@@ -1,1 +1,54 @@
 # dictionaries.py
+
+def demo():
+    """
+    Demonstrate basic dictionary stuff
+    """
+    myDictionary = {"Cincinnati":"Bearcats", "Xavier":"Musketeers", "NKU":"Norse", "UC Clermont":"Cougars"}
+    print(myDictionary)
+
+    # Iterate over the dictionary by key
+    for key in myDictionary.keys():
+        print(key)    
+    
+    # Iterate by key/value
+    for item in myDictionary.items():
+        print(item)
+        
+    # Iterate by value
+    for value in myDictionary.values():
+        print(value)
+        
+    # Add a key/value pair to the dictionary
+    myDictionary["Michigan State"] = "Spartans" 
+    
+    print(len(myDictionary))
+
+    # Cause an exception. Add try / except logic to
+    # gracefully handle this.
+    try:
+        print(myDictionary["Ohio State"])
+    except:
+        # We end up here if an exception is thrown
+        # in the try block
+        print("Ohio State is an invalid key")
+    
+    # Remove Xavier by key and print the entire dictionary
+    myDictionary.pop("Xavier")
+    print(myDictionary)
+    
+    # Create another dictionary called newTeams.
+    # Add several key/value pairs
+    # Combine that with myDictionary and print the results
+    newTeams = {"Indiana":"Hoosiers", 
+                "Hillsdale":"Chargers", 
+                "Toledo":"Rockets"} 
+    """
+    # Brute force. Inelegant
+    for key in newTeams.keys():
+        myDictionary[key] = newTeams[key]
+    print(myDictionary)
+    """
+    myDictionary.update(newTeams)
+    print(myDictionary)
+    
--- InClass20240924/mainPackage/main.py
+++ InClass20240924/mainPackage/main.py
@@ -1,2 +1,6 @@
 # main.py
+from functionPackage.dictionaries import *
 
+if __name__ == "__main__":
+    print("I am main.py")
+    demo()
